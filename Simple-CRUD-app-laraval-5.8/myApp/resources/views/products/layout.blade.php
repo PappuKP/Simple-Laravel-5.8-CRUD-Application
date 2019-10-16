8b5648d457b6b5171d140756348c013a7f778da1
Pappu <pappuskskumar@gmail.com>
2019-10-17 01:41:40 +0530

    first commit

---
+++ Simple-CRUD-app-laraval-5.8/myApp/resources/views/products/layout.blade.php
@@ -0,0 +1,29 @@
+<!-- this is our generic file write any html css and any thing -->
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Laravel 5.8 CRUD Application </title>
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.4/css/bootstrap.css" rel="stylesheet">
+
+	
+
+</head>
+
+<body>
+
+
+	<div class="container">
+
+		@yield('content')
+		
+	</div>
+
+
+</body>
+
+</html>
